e94822263d3a8d36da4723415008c17911987eae
alex-bramwell <alex.s.bramwell.86@gmail.com>
2025-07-03 17:48:28 +0000

    Refactor Home component layout to use grid structure for improved responsiveness

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,15 @@ import Button from '../components/Button';
 
 export default function Home() {
   return (
-    <div>
-      <h1>My Portfolio</h1>
-      <Button>Click Me</Button>
+    <div className="grid-container">
+      <div className="grid-row">
+        <div className="grid-col-12 grid-col-md-8">
+          <h1>My Portfolio</h1>
+        </div>
+        <div className="grid-col-12 grid-col-md-4">
+          <Button>Click Me</Button>
+        </div>
+      </div>
     </div>
   );
 }
